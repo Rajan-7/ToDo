eeb20d50000cb6fd01cc3675bd1ee1ef0c955343
Rajan mabo limbu <rajanmabolimbu@kcc.edu.np>
2022-10-28 19:11:30 +0545

    Task adding function

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-const forms=document.querySelector()
+const forms=document.querySelector( "#formid");
+let arry=[];
+const forminput=document.querySelector("#form1");
+// console.log(forminput,forms);
+forms.addEventListener("submit",(event)=>{
+    event.preventDefault();
+    const task=forminput.value;
+    if(task === '' || task===null) return;
+    const newtask={
+        id:Date.now(),
+        text:task
+    } 
+    arry.push(newtask);
+    forminput.value='';
+    // console.log(arry);
+     
+}
+)
